c1e8abbd1a779c2649fadd199d3ec08207937363
ckeller22 <chriskeller222@gmail.com>
2019-09-06 20:55:40 -0700

    implementation of SendGrid contact form, JavaScript/PHP validation

--- app.js
+++ app.js
@@ -1,19 +1,62 @@
-/*function handleMail() {
-	var form = $("#contact-form");
-	console.log("mailbutton");
+function handleMail() {
+	var name = $("#name").val();
+	var email = $("#email").val();
+	var desc = $("#description").val();
 
-	$(form).submit(function(event) {
-		event.preventDefault();
-	});
+	var alertMessage = "";
 
-	var formData = $(form).serialize();
-	console.log(formData);
-	$.ajax({
-		type: "POST",
-		url: $(form).attr("action"),
-		data: formData
-	});
-}*/
+	if (name.length <= 0) {
+		alertMessage += "Please enter your name. \n";
+	}
+	if (!validateEmail(email)) {
+		alertMessage += "Please enter a valid email address. \n";
+	}
+	if (desc.length <= 0) {
+		alertMessage += "Please tell me about your project. \n";
+	}
+	if (alertMessage.length > 0) {
+		alert(alertMessage);
+	} else {
+		$.ajax({
+			type: "POST",
+			url: "handleMail.php",
+			data: {
+				name: name,
+				email: email,
+				desc: desc
+			},
+
+			success: function(responseCode) {
+				if (responseCode == 202) {
+					hideForm();
+				} else {
+					alert(
+						"Unfortunately an error has occured. Please wait and try again. \n" +
+							responseCode
+					);
+				}
+			},
+			failure: function(data) {
+				alert(
+					"Unfortunately an error has occurred. Please wait and try again."
+				);
+			}
+		});
+	}
+}
+
+function hideForm() {
+	$("#name").hide();
+	$("#email").hide();
+	$("#description").hide();
+	$("#submit").hide();
+	$("#emailSent").show("fast");
+}
+
+function validateEmail(email) {
+	var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
 
 //scroll magic
 
--- handleMail.php
+++ handleMail.php
@@ -1,12 +1,56 @@
-<?
-$name = $_POST['name'];
-$from = $_POST["email"];
-$message = $_POST["description"];
-$to = "chriskeller222@gmail.com";
-$subject = "portfolio contact";
+<?php
 
-$headers = 'From: <$from>' . "\r\n";
+require 'vendor/autoload.php';
 
-mail($to, $subject, $message);
+$myEmail = getenv("MY_EMAIL");
+
+//sanitize and validate POST data
+$errorMessage = "";
+
+if (empty($_POST["name"])) {
+    $errorMessage .= "Please enter a name. \n";
+} else {
+    $name = filter_var($_POST["name"]);
+}
+if (empty($_POST["email"]) || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+    $errorMessage .= "Please enter a valid email address. \n";   
+} else {
+    $email = filter_var($_POST["email"]);
+}
+if (empty($_POST["desc"])) {
+    $errorMessage .= "Please tell me about your project. \n";
+} else {
+    $desc = filter_var($_POST["desc"]);
+}
+
+// if errorMessage length > 0, breaks and echos errormessage, otherwise sends mail
+if (strlen($errorMessage) > 0) {
+    echo $errorMessage;
+} else {
+    $from = new SendGrid\Email(null, $email);
+    $subject = $name;
+    $to = new SendGrid\Email(null, $myEmail);
+    $content = new SendGrid\Content("text/plain", $desc);
+    $mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+    $apiKey = getenv('SENDGRID_API_KEY');
+    $sg = new \SendGrid($apiKey);
+
+    try {
+        $response = $sg->client->mail()->send()->post($mail);
+        $JSONResponse = json_encode($response->statusCode());
+        
+        echo $JSONResponse;
+
+    } catch (Exception $e) {
+        echo 'Caught exception: '.  $e->getMessage(). "\n";
+    }
+}
+
+
+
+
+
+
 
 ?>
